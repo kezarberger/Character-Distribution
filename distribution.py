662eab91d26108878ecae21ad8e33daa93132811
kezarberger <kezarberger@hanovernorwichschools.org>
2016-11-01 13:59:58 -0400

    Updated from Brython Server: 11/1/2016 1:59:58 PM

--- distribution.py
+++ distribution.py
@@ -45,17 +45,17 @@ for a in alph:
         sat = ''
         for y in range(geronimo):
             sat = sat + (a)
-        print (sat)
-         meow.append(sat)
+        meow.append(sat)
         meow = list(meow)
-    for i in range(26):
-        jum = 0
-        while jum < len(meow):
-            if len(meow[jum]) < len(meow[jum+1]):
-                wad = meow[jum]
-                meow[jum] = meow[jum+1]
-                meow[jum+1] = wad
-                jum += 1
-    for c in meow:
-        print (c)
+print(meow)
+
+for i in range(26):
+    jum = 0
+    while jum < len(meow):
+        if len(meow[jum]) < len(meow[jum+1]):
+            wad = meow[jum]
+            meow[jum] = meow[jum+1]
+            meow[jum+1] = wad
+            jum += 1
+
        
